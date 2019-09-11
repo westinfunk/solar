0bfe52a96d40c95ffad030370069ecb4d9666019
westin <westinfunk@gmail.com>
2019-09-11 10:52:35 -0600

    build sidebar

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+const Header = (props) => (
+    <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 mt-5 border-bottom">
+        <h1>{props.title}</h1>
+    </div>
+)
+
 class Main extends React.Component {
     render() {
         return (
-            <div />
+            <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+                    <Header title={"Accounts"} />
+            </main>
         )
     }
 }
--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHammer, faAd, faAddressCard } from '@fortawesome/free-solid-svg-icons';
+
+class NavLink {
+    constructor(name, isActive, link, icon) {
+        this.name = name;
+        this.isActive = isActive;
+        this.link = link;
+        this.icon = icon;
+    }
+}
+
+const navLinks = [
+    new NavLink("Projects", true, "#", faHammer), 
+    new NavLink("Accounts", false, "pots", faAddressCard), 
+    new NavLink("Inactive", false, "#", faAd)
+];
+
+const NavLinkItem = function(navLink) {
+    let linkClass = navLink.isActive ? "nav-link active" : "nav-link";
+    return (
+        <li className="nav-item">
+        <a className={linkClass} href={navLink.link}>
+            <FontAwesomeIcon icon={navLink.icon}/>{" " + navLink.name}</a>
+        </li>
+    )
+}
 
 class Sidebar extends React.Component {
     render() {
         return (
-            <div />
+            <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+                <div className="sidebar-sticky">
+                    <ul className="nav flex-column">
+                        {navLinks.map((navLink, index) => NavLinkItem(navLink))}
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
