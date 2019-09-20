ef155b5e273ba4c7d4a9657f824e6ae22da83926
westin <westinfunk@gmail.com>
2019-09-19 20:46:03 -0600

    text field changes PDF text

---
+++ src/containers/PDF/DocumentGenerator.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Page, Text, View, Document, Link, StyleSheet, PDFViewer, Image, Font } from '@react-pdf/renderer';
+
+
+const DocumentGenerator = ({ text }) => {
+    return(
+        <Document>
+            <Page style={styles.body}>
+            <Text style={styles.text}>
+            {text}
+            </Text>
+            <Text style={styles.pageNumber} render={({ pageNumber, totalPages }) => (
+        `${pageNumber} / ${totalPages}`
+      )} fixed />
+            </Page>
+        </Document>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    body: {
+      paddingTop: 35,
+      paddingBottom: 65,
+      paddingHorizontal: 35,
+    },
+    title: {
+      fontSize: 24,
+      textAlign: 'center',
+      fontFamily: 'Oswald'
+    },
+    author: {
+      fontSize: 12,
+      textAlign: 'center',
+      marginBottom: 40,
+    },
+    subtitle: {
+      fontSize: 18,
+      margin: 12,
+      fontFamily: 'Oswald'
+    },
+    text: {
+      margin: 12,
+      fontSize: 14,
+      textAlign: 'justify',
+      fontFamily: 'Times-Roman'
+    },
+    image: {
+      marginVertical: 15,
+      marginHorizontal: 100,
+    },
+    header: {
+      fontSize: 12,
+      marginBottom: 20,
+      textAlign: 'center',
+      color: 'grey',
+    },
+    pageNumber: {
+      position: 'absolute',
+      fontSize: 12,
+      bottom: 30,
+      left: 0,
+      right: 0,
+      textAlign: 'center',
+      color: 'grey',
+    },
+  });
+  
+  Font.register({
+    family: 'Oswald',
+    src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
+  });
+
+
+export default DocumentGenerator;
